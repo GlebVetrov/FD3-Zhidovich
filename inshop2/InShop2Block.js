46efa0c76680323688d84ac6bd34463588f18013
BigGlebovsky <3696732@mail.ru>
2019-04-13 17:33:54 +0300

    fix select

--- inshop2/InShop2Block.js
+++ inshop2/InShop2Block.js
@@ -1,93 +1,47 @@
 var InShop2Block = React.createClass({
 
     displayName: 'InShop2Block',
+
+    propTypes: {
+      product: React.PropTypes.arrayOf(
+        React.PropTypes.object.isRequired)
+    },
     
     getInitialState: function () {
       return {
-        product: [
-          {code: 1, delete: false}, 
-          {code: 2, delete: false},
-          {code: 3, delete: false}, 
-          {code: 4, delete: false},
-          {code: 5, delete: false},
-        ],
-
-        productData: [
-          {
-          className: 'InShop2Block_product',
-          name: 'product1',
-          prise: 30,
-          url: 'http://product1.com',
-          quantity: 20,
-        },
-
-        {
-          className: 'InShop2Block_product',
-          name: 'product2',
-          prise: 15,
-          url: 'http://product2.com',
-          quantity: 20,  
-        }
-        ,
-
-        {
-          className: 'InShop2Block_product',
-          name: 'product3',
-          prise: 45,
-          url: 'http://product3.com',
-          quantity: 120,  
-        }
-        ,
-
-        {
-          className: 'InShop2Block_product',
-          name: 'product4',
-          prise: 55,
-          url: 'http://product4.com',
-          quantity: 220,  
-        }
-        ,
-
-        {
-          className: 'InShop2Block_product',
-          name: 'product5',
-          prise: 25,
-          url: 'http://product5.com',
-          quantity: 520,  
-        }
-      ],
-
-      productLast: 0,
-
+        productData: this.props.product.slice(),
+        productLast: null,
       }
     },
 
     select: function(num) {      
       let sel = this.state.productData.slice();
-      let last = this.state.productLast;
-      sel[last].className = 'InShop2Block_product';      
-      sel[num].className = 'InShop2Block_product focused';      
+      sel.forEach(element => {
+        element.className = 'InShop2Block_product';
+      });
+      sel[num].className = 'InShop2Block_product focused';
       this.setState({productData: sel, productLast: num})
     },
 
-    delete: function(num) {
-      let del = this.state.product.slice();      
-      del[num].delete = true;
+    delete: function(EO, num) {      
+      let del = this.state.productData;      
+      delete del[num];
       this.setState({product: del})
+      EO.stopPropagation();
     },
 
     render: function() {
-
-      var productList = this.state.product.map( (v, i) => {
-        if (v.delete == false) {                    
+      
+      var productList = this.state.productData.map( (v,i) => {
+                            
           return React.createElement(product, {
              key: v.code, 
-             data: this.state.productData[i],
+             data: v,
              cbDelete: this.delete,
              cbSelect: this.select,
              num: i,
             });
-      }
+      
       }
       )
   
@@ -131,9 +85,9 @@ var InShop2Block = React.createClass({
       this.props.cbSelect(num);
     },
 
-    delete: function() {
+    delete: function(EO) {
       let num = this.props.num;      
-      this.props.cbDelete(num);
+      this.props.cbDelete(EO, num);
     },
 
     render: function() {
