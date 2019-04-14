ae6700f53c14ccfb8ea45b3ecd7ce7bcf50d6dcc
BigGlebovsky <3696732@mail.ru>
2019-04-14 13:54:30 +0300

    fix

--- filter/FilterBlock.js
+++ filter/FilterBlock.js
@@ -11,74 +11,64 @@ var FilterBlock = React.createClass({
           ),
     },
     
-    changeStatus : function() {
-        if (this.state.checkbox == true) {
-            this.notSortWords();                    
-            this.setState({checkbox: false,});
-            return;
-        }  else   {
-            this.sortWords();
-            this.setState({checkbox: true,});
-            return;
-        }
-
-    },  
-
-    notSortWords: function() {
-        this.setState({words: this.props.text.map(function (item) {
-            return item;
-        }),});
-        return; 
-    },
-
-    sortWords: function() {
-        this.setState({words: this.state.words.sort(function (a, b) {
-            if (a.text > b.text) {
-              return 1;
-            }
-            if (a.text < b.text) {
-              return -1;
-            }
-            return 0;
-          }),});
-        
-    },
-
-    clearField: function() {
-        this.setState({text: ''});
-        this.setState({checkbox: false,});
-        this.notSortWords();
-        return;
-    },
-
     getInitialState: function() {
         return {
-            text:'',
-            words: this.props.text.map(function (item) {
-                return item;
-            }),            
-            checkbox: false,
-    }
+            sorted: false,
+            filtered: '',
+            processStr: this.props.text.slice(),
+            }
+    },
+    
+    processList: function(){
+        let res = this.props.text;
+        let sort = this.state.sorted;
+        let filter = this.state.filtered;
+        if(filter != '') {
+            res = res.filter((s) => s.text.indexOf(filter) != -1)
+        } else {
+            res = this.props.text.slice();
+        }
+
+        if (sort === true) {
+            res.sort(function (a, b) {
+                if (a.text > b.text) {
+                  return 1;
+                }
+                if (a.text < b.text) {
+                  return -1;
+                }
+                return 0;
+              })
+        }
+        this.setState({processStr: res});
+    },
+   
+    changeStatus : function(EO) {
+        this.setState({sorted: EO.target.checked}, this.processList);
+    },  
+
+    clearField: function() {
+        this.setState({
+            sorted: false,
+            filtered: '',
+        }, this.processList);
     },
 
-    checkAnswer: function(EO) {
-        this.setState({text: EO.target.value});        
-        return;
+    checkAnswer: function(EO) {        
+        this.setState({filtered: EO.target.value}, this.processList);        
     },
 
     render: function() {
 
-        var selectText = this.state.words.map( (v) => {                
-                if (v.text.indexOf(this.state.text) != -1) {
-                return React.DOM.option ({key: v.code}, v.text)
-            }
+        var selectText = this.state.processStr.map( (v) => {
+         return React.DOM.option ({key: v.code}, v.text)
         }
                 
          );
 
         return React.DOM.div ({className: 'FilterBlock'},
-        React.DOM.input ({className: 'FilterBlock_checkbox', type: 'checkbox', onClick: this.changeStatus, checked: this.state.checkbox}),
-        React.DOM.input ({className: 'FilterBlock_text', type: 'text', value: this.state.text, onChange: this.checkAnswer}),
+        React.DOM.input ({className: 'FilterBlock_checkbox', type: 'checkbox', checked: this.state.sorted, onClick: this.changeStatus}),
+        React.DOM.input ({className: 'FilterBlock_text', type: 'text', value: this.state.filtered, onChange: this.checkAnswer}),
         React.DOM.input ({className: 'FilterBlock_button', type: 'button', value: 'Сброс', onClick: this.clearField}),
         React.DOM.br (),
         React.DOM.select ({className: 'FilterBlock_multiple', multiple: true, size: '5', },
