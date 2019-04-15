fb42ccb54f9a7190553c0b976a2f276cd0001f52
BigGlebovsky <3696732@mail.ru>
2019-04-15 21:07:20 +0300

    add localeCompare into sort

--- filter/FilterBlock.js
+++ filter/FilterBlock.js
@@ -31,13 +31,14 @@ var FilterBlock = React.createClass({
 
         if (sort === true) {
             res.sort(function (a, b) {
-                if (a.text > b.text) {
-                  return 1;
-                }
-                if (a.text < b.text) {
-                  return -1;
-                }
-                return 0;
+                return a.text.localeCompare(b.text);
+                // if (a.text > b.text) {
+                //   return 1;
+                // }
+                // if (a.text < b.text) {
+                //   return -1;
+                // }
+                // return 0;
               })
         }
         this.setState({processStr: res});
