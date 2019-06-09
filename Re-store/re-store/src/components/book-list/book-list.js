ca615098efc1734eff972710e9b13c2a0301a7cc
BigGlebovsky <3696732@mail.ru>
2019-06-09 08:48:52 +0300

    add new componet to redux

--- Re-store/re-store/src/components/book-list/book-list.js
+++ Re-store/re-store/src/components/book-list/book-list.js
@@ -2,31 +2,31 @@ import React from 'react';
 import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 
-import { booksLoaded, booksRequested, booksError } from '../../actions';
+import { fetchBooks } from '../../actions';
 import { withBookstoreService } from '../hoc';
 import compose from  '../../utils';
 import './book-list.css';
 import Spinner from "../spinner";
 import ErrorIndicator from "../error-indicator";
 
-class BookList extends React.Component {
+const BookList = ({ books }) => {
+    return (
+        <ul className='book-list'>
+            {
+                books.map((book) => {
+                    return (<li key={book.isbn}>
+                                <BookListItem book = { book } />
+                            </li>)
+                })
+            }
+        </ul>
+    )
+};
+
+class BookListContainer extends React.Component {
     
     componentDidMount() {
-        //1. получить данные
-        const { bookstoreService,
-                booksLoaded,
-                booksRequested,
-                booksError} = this.props;
-
-        booksRequested();
-        bookstoreService.getBooks()
-            .then((data) => {
-                booksLoaded(data);
-            })
-            .catch((err) => booksError(err));
-
-        //2. передать(dispatch) действие в store
-
+        this.props.fetchBooks();
     }
     
     render() {
@@ -39,17 +39,7 @@ class BookList extends React.Component {
             return <ErrorIndicator/>
         }
 
-        return (
-            <ul className='book-list'>
-                {
-                    books.map((book) => {                                            
-                        return (<li key={book.isbn}>
-                                    <BookListItem book = { book } />
-                                </li>)
-                    })
-                }
-            </ul>
-        )
+        return <BookList books={ books } />;
     }
 }
 
@@ -57,14 +47,15 @@ const mapStateToProps = ({ books, loading, error }) => {
     return { books, loading, error }
 };
 
-const mapDispatchToProps = {
-    booksLoaded,
-    booksRequested,
-    booksError
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const { bookstoreService } = ownProps;
+    return {
+        fetchBooks: fetchBooks(bookstoreService, dispatch)
+    }
 };
 
 //подключение connect HOC из redux к BookList каие данные буду получать из redux-store
 export default compose( 
     withBookstoreService(),
     connect(mapStateToProps, mapDispatchToProps)
-    )(BookList);
+    )(BookListContainer);
